0e4c31cf1c27b3a7b1b3e64c26572ce932c38543
wngus457 <wngus457@gmail.com>
2023-03-03 22:32:13 +0900

    init project

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,8 @@
+object AppConfig {
+    const val applicationId = "com.juhyeon.composemvi"
+    const val compileSdk = 33
+    const val minSdk = 28
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
